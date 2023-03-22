71f101dae88adf08f39253398f96b4bb6decc178
Maurício Bernardo <mauriciobernardo.mbd@gmail.com>
2023-03-22 17:37:27 -0300

    Finaliza a atividade

---
+++ MeuPrograma.java
@@ -0,0 +1,54 @@
+/* QUESTÃO 3
+public class MeuPrograma {
+
+    public static void main (String[] args) {
+        System.out.println("Terminei a primeira aula com um programa Java!");
+    }
+
+} */
+
+/* QUESTÃO 5
+
+public class MeuPrograma {
+
+    public static void start (String[] args) {
+        System.out.println("Terminei a primeira aula com um programa Java!");
+    }
+
+} */
+
+/* QUESTÃO 6
+
+public class MeuPrograma2 {
+
+    public static void main (String[] args) {
+        System.out.println("Mauricio");
+        System.out.println("Flamengo");
+    }
+
+}
+*/
+
+/* QUESTÃO 7
+*/
+PUBLIC CLASS MEUPROGRAMA2 {
+
+    PUBLIC STATIC VOID MAIN (STRING[] ARGS) {
+        SYSTEM.OUT.PRINTLN("MAURICIO");
+        SYSTEM.OUT.PRINTLN("FLAMENGO");
+    }
+}
+
+
+/* QUESTÃO 8
+
+public class Programa {
+
+    public static void main (String[] args) {
+        System.out.println("Mauricio");
+        System.out.println("Flamengo");
+    }
+
+}
+
+*/ 
